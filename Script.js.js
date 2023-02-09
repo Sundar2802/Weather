44de2fcc76aad58e79cb2032f9e26c6c8ae6e41d
sundar_2802 <124782589+Sundar2802@users.noreply.github.com>
2023-02-09 13:45:06 +0530

    Add files via upload

---
+++ Script.js.js
@@ -0,0 +1,18 @@
+
+function displayresult(){
+    var city=document.getElementById("city").value;
+    var apikey='9517712d3ace9ab613935103b79f73e1';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apikey+'&units=metric')
+        .then(response => response.json())
+        .then(data =>{
+            var t= data['main']['temp']  
+            var maxt= data['main']['temp_max']
+            var mint= data['main']['temp_min']
+            var humidity= data['main']['humidity']
+            document.getElementById("cityname").innerHTML=city;
+            document.getElementById("temp").innerHTML=t+" deg C";
+            document.getElementById("maxtemp").innerHTML=maxt+" deg C";
+            document.getElementById("mintemp").innerHTML=mint+" deg C";
+            document.getElementById("hum").innerHTML=humidity+;
+        })
+        }
